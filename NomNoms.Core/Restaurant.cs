4626bb2d1a367475bf2f45c5890abe233c8a2a9b
Tony Wang <zichutonywang@outlook.com>
2019-07-09 14:47:15 -0400

    Added constructor and dummy data to InMemoryRestaurantData().

--- NomNoms.Core/Restaurant.cs
+++ NomNoms.Core/Restaurant.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace NomNoms.Core
 {
-    class Restaurant
+    public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- NomNoms.Data/IRestaurantData.cs
+++ NomNoms.Data/IRestaurantData.cs
@@ -7,6 +7,26 @@ namespace NomNoms.Data
 {
      public interface IRestaurantData
     {
-        IEnumerable<Restaurant>
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            { 
+                new Restaurant { Id = 1, Name = "Bunni's nommy noms", Location = "Clubhouse", Cuisine = CuisineType.Mexican}
+                new Restaurant { Id = 2, Name = "Poni's nomlicious oms", Location = "Cloudy loudy", Cuisine = CuisineType.None}
+                new Restaurant { Id = 3, Name = "Bun's buns", Location = "Secret bush", Cuisine = CuisineType.Chinese}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+
+        }
     }
 }
