2b9cbfa00b79734195bae61a6cab2d2ddd7f4ab4
Tony Wang <zichutonywang@outlook.com>
2019-07-11 13:28:45 -0400

    Searching is working using model binding.

--- NomNoms/Pages/Restaurants/List.cshtml.cs
+++ NomNoms/Pages/Restaurants/List.cshtml.cs
@@ -25,10 +25,10 @@ namespace NomNoms.Pages.Restaurants
             this.restaurantData = restaurantData;
         }
 
-        public void OnGet()
+        public void OnGet(string searchTerm)
         {
             Message = config["Message"];
-            Restaurants = restaurantData.GetAll();
+            Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
         }
     }
 }
