0921afccd9a07632d62820503a1df79da82e6da6
Tony Wang <zichutonywang@outlook.com>
2019-07-15 14:16:50 -0400

    Starting with EFC.

---
+++ NomNoms.Data/NomNomsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NomNoms.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NomNoms.Data
+{
+    public class NomNomsDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
