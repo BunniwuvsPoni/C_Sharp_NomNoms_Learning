b9361232f71395219fd82985558734c527f7472f
Tony Wang <zichutonywang@outlook.com>
2019-08-07 09:06:07 -0400

    Demoed scaffolded views.

---
+++ NomNoms/Pages/R2/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NomNoms.Core;
+using NomNoms.Data;
+
+namespace NomNoms.Pages.R2
+{
+    public class CreateModel : PageModel
+    {
+        private readonly NomNoms.Data.NomNomsDbContext _context;
+
+        public CreateModel(NomNoms.Data.NomNomsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Restaurants.Add(Restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ NomNoms/Pages/R2/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using NomNoms.Core;
+using NomNoms.Data;
+
+namespace NomNoms.Pages.R2
+{
+    public class IndexModel : PageModel
+    {
+        private readonly NomNoms.Data.NomNomsDbContext _context;
+
+        public IndexModel(NomNoms.Data.NomNomsDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
