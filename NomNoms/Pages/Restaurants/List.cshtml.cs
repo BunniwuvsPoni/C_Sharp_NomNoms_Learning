b40b29f968db918f2c9fd875e8aaa94864f2916b
Tony Wang <zichutonywang@outlook.com>
2019-08-22 15:49:40 -0400

    Testing logging.

--- NomNoms/Pages/Restaurants/List.cshtml.cs
+++ NomNoms/Pages/Restaurants/List.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using NomNoms.Core;
 using NomNoms.Data;
 
@@ -14,6 +15,7 @@ namespace NomNoms.Pages.Restaurants
     {
         private readonly IConfiguration config;
         private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
 
         public string Message { get; set; }
         public IEnumerable<Restaurant> Restaurants { get; set; }
@@ -23,14 +25,17 @@ namespace NomNoms.Pages.Restaurants
         public string SearchTerm { get; set; }
 
         public ListModel(IConfiguration config,
-                         IRestaurantData restaurantData)
+                         IRestaurantData restaurantData,
+                         ILogger<ListModel> logger)
         {
             this.config = config;
             this.restaurantData = restaurantData;
+            this.logger = logger;
         }
 
         public void OnGet(string searchTerm)    //  Model binding happens here from the form
         {
+            logger.LogError("Executing ListModel");
             Message = config["Message"];
             Restaurants = restaurantData.GetRestaurantsByName(searchTerm);
         }
